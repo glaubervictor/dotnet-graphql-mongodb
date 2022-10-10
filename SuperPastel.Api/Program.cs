2787db1eb95bbedac5ad33ba2f96cfc22d29560b
Glauber Carvalho <glaubervictor@gmail.com>
2022-10-10 13:15:12 -0300

    Configuração porta Railway

--- SuperPastel.Api/Program.cs
+++ SuperPastel.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace SuperPastel.Api
 {
@@ -15,6 +16,16 @@ namespace SuperPastel.Api
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
+                    // For running in Railway
+                    var portVar = Environment.GetEnvironmentVariable("PORT");
+                    if (portVar is { Length: > 0 } && int.TryParse(portVar, out int port))
+                    {
+                        webBuilder.ConfigureKestrel(options =>
+                        {
+                            options.ListenAnyIP(port);
+                        });
+                    }
                 });
     }
 }
